4a160e625a3b15043cc88b3d1a30b2f08fa7619f
Timoty Darrel Davidson <c14230235@john.petra.ac.id>
2025-12-05 08:54:16 +0700

    22-26 Done

--- app/src/main/java/paba/c14230235/myfirstroom/MainActivity.kt
+++ app/src/main/java/paba/c14230235/myfirstroom/MainActivity.kt
@@ -1,12 +1,22 @@
 package paba.c14230235.myfirstroom
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import paba.c14230235.myfirstroom.database.NoteRoomDatabase
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var DB : NoteRoomDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,21 @@ class MainActivity : ComponentActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        DB = NoteRoomDatabase.getDatabase(this)
+
+        val _fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+        _fabAdd.setOnClickListener {
+            startActivity(Intent(this, TambahData::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        CoroutineScope(Dispatchers.Main).async {
+            val note = DB.funNoteDao().selectAll()
+            Log.d("data ROOM", note.toString())
         }
     }
 }
