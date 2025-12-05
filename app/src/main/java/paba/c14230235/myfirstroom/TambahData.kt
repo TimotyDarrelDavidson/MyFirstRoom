81b9ca3579490f57e13a35c8f8ece1a2084bb1a8
Timoty Darrel Davidson <c14230235@john.petra.ac.id>
2025-12-05 13:51:33 +0700

    Finish

--- app/src/main/java/paba/c14230235/myfirstroom/TambahData.kt
+++ app/src/main/java/paba/c14230235/myfirstroom/TambahData.kt
@@ -10,13 +10,15 @@ import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 import paba.c14230235.myfirstroom.database.Note
 import paba.c14230235.myfirstroom.database.NoteRoomDatabase
 import paba.c14230235.myfirstroom.helper.DateHelper.getCurrentDate
 
 class TambahData : AppCompatActivity() {
 
-    val DB = NoteRoomDatabase.getDatabase(this)
+    private lateinit var DB : NoteRoomDatabase
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +29,8 @@ class TambahData : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        DB = NoteRoomDatabase.getDatabase(this)
 
         var tanggal : String = getCurrentDate()
 
@@ -45,9 +49,47 @@ class TambahData : AppCompatActivity() {
                         tanggal
                     )
                 )
+                withContext(Dispatchers.Main) {
+                    finish()
+                }
             }
         }
 
+        var iID : Int = 0
+        var iAddEdit : Int = 0
 
+        iID = intent.getIntExtra("noteId", 0)
+        iAddEdit = intent.getIntExtra("addEdit", 0)
+
+        if (iAddEdit == 0) {
+            _btnTambah.visibility = Button.VISIBLE
+            _btnUpdate.visibility = Button.GONE
+            _etJudul.isEnabled = true
+        } else {
+            _btnTambah.visibility = Button.GONE
+            _btnUpdate.visibility = Button.VISIBLE
+            _etJudul.isEnabled = false
+
+            CoroutineScope(Dispatchers.IO).async {
+                val noteItem = DB.funNoteDao().getNote(iID)
+                withContext(Dispatchers.Main) {
+                    _etJudul.setText(noteItem.judul)
+                    _etDeskripsi.setText(noteItem.deskripsi)
+                }
+            }
+        }
+
+        _btnUpdate.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.funNoteDao().update(
+                    _etJudul.text.toString(),
+                    _etDeskripsi.text.toString(),
+                    iID
+                )
+                withContext(Dispatchers.Main) {
+                    finish()
+                }
+            }
+        }
     }
 }
